18864592015aedc2af4d41a1ae636289d81a9be6
msperlin <marceloperlin@gmail.com>
2023-03-09 16:41:05 -0300

    site compilation at 20230309 164104

--- compile_and_publish.sh
+++ compile_and_publish.sh
@@ -3,7 +3,7 @@
 export RSTUDIO_PANDOC=/usr/lib/rstudio/bin/pandoc
 
 #MYDIR="/home/msperlin/GitRepo/03-sites/msperlin-blog"
-MYDIR="/home/msperlin/GitRepo/02-sites/msperlin-blog"
+MYDIR="/home/msperlin/GitRepo/02-websites/msperlin-blog"
 PATHBOOK="$MYDIR/public/" 
 
 cd $MYDIR
